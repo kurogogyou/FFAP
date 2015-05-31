ebe34420ee83ccaf9c268ce29bcdb65dec94c52d
Mario J. Lara <mario.lara1289@gmail.com>
2015-05-30 20:59:20 -0400

    Catalog Display

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,13 @@
 class Product < ActiveRecord::Base
 	validates :title, :description, :image_url, presence:true
-	validates :title, uniqueness: true
+	validates :title, uniqueness: true #, format: {message: 'Title field data must be unique.'}
 	validates :title, length: {minimum: 4}
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :image_url, allow_blank: true, format: {
 		with:  %r{\.(gif|jpg|png)\Z}i,
 		message: 'must be a URL for GIF, JPG, or PNG image.' 
 	}
+	def self.latest
+		Product.order(:updated_at).last 
+	end
 end
