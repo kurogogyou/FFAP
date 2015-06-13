ac28b986e128ebf205b17d62eb6e462983f67554
Denisse Lara <dmlaramartin@gmail.com>
2015-06-12 20:55:22 -0400

    Fix typo in jquery select

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,8 +1,8 @@
 class StoreController < ApplicationController
   include CurrentCart
   before_action :set_cart
+
   def index
   	@products = Product.order(:title)
-  	session[:visit_count] = 0
   end
 end
