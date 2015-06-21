bd2cf074a1653b6ebd2d188b033649343d95c4e1
Mario J. Lara <mario.lara1289@gmail.com>
2015-06-21 10:18:31 -0400

    debugging heroku db 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Product.delete_all
-Product.all.destroy
 #...
 
 #Product.create!(
