21539c5a02597df364f00ece54a50a12880fd227
Mario J. Lara <mario.lara1289@gmail.com>
2015-06-21 10:35:39 -0400

    seeding heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,59 +8,60 @@
 Product.delete_all
 #...
 
-#Product.create!(
-#	title: 'Retrovisor',
-#	description: 
-#		%{<p>
-#			Retrovisor Honda Civic 2007
-#		</p>},
-#	image_url: 'retro.jpg',
-#	price: 12.5)
-#Product.create!(
-#	title: 'Luz Trasera',
-#	description: 
-#		%{<p>
-#			Luz trasera Toyota Corolla 2003
-#		</p>},
-#	image_url: 'Luz trasera.jpg',
-#	price: 25.0)
+Product.create!(
+	title: 'Retrovisor',
+	description: 
+		%{<p>
+			Retrovisor Honda Civic 2007
+		</p>},
+	image_url: 'retro.jpg',
+	price: 12.5)
+Product.create!(
+	title: 'Luz Trasera',
+	description: 
+		%{<p>
+			Luz trasera Toyota Corolla 2003
+		</p>},
+	image_url: 'Luz trasera.jpg',
+	price: 25.0)
 
-#Product.create!(
-#	title: 'Bumper frontal',
-#	description: 
-#		%{<p>
-#			Bumper Frontal Lexus IS 250 2009
-#		</p>},
-#	image_url: 'Bumper_Delantero.jpg',
-#	price: 81.73)
+Product.create!(
+	title: 'Bumper frontal',
+	description: 
+		%{<p>
+			Bumper Frontal Lexus IS 250 2009
+		</p>},
+	image_url: 'Bumper_Delantero.jpg',
+	price: 81.73)
 
-#Product.create!(
-#	title: 'Luz Delantera',
-#	description: 
-#		%{<p>
-#			Luz Delantera Mitsubishi Lancer 2008
-#		</p>},
-#	image_url: 'luz_delan.jpg',
-#	price: 355.17)
+Product.create!(
+	title: 'Luz Delantera',
+	description: 
+		%{<p>
+			Luz Delantera Mitsubishi Lancer 2008
+		</p>},
+	image_url: 'luz_delan.jpg',
+	price: 355.17)
 
-#Product.create!(
-#	title: 'Halogenos',
-#	description: 
-#		%{<p>
-#			Halogenos Volkswagen Jetta 2011
-#		</p>},
-#	image_url: 'Halogenos.jpg',
-#	price: 122.79)
+Product.create!(
+	title: 'Halogenos',
+	description: 
+		%{<p>
+			Halogenos Volkswagen Jetta 2011
+		</p>},
+	image_url: 'Halogenos.jpg',
+	price: 122.79)
 
-#Product.create!(
-#	title: 'Bumper Trasero',
-#	description: 
-#		%{<p>
-#			Bumper Trasero Toyota Camry 2012
-#		</p>},
-#	image_url: 'Bumper_Trasero.jpg',
-#	price: 69.30)
+Product.create!(
+	title: 'Bumper Trasero',
+	description: 
+		%{<p>
+			Bumper Trasero Toyota Camry 2012
+		</p>},
+	image_url: 'Bumper_Trasero.jpg',
+	price: 69.30)
 
+#........
 #Product.create!(
 #	title: 'Programming Ruby 1.9 & 2.0',
 #	description: 
