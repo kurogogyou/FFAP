b21cc65c712839e3fb9455da401a46eba1a68671
Mario J. Lara <mario.lara1289@gmail.com>
2015-06-30 14:09:00 -0400

    Testing Email -H1, failing

--- config/environments/environment.rb
+++ config/environments/environment.rb
@@ -2,12 +2,12 @@ Depot::Application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-  	address: 		"smtp.gmail.com",
-  	port: 			587,
-  	domain: 		"ffap-itt-2015",
-  	authentication: "plain",
-  	user_name: 		"ffap.itt.2015@gmail.com",
-  	password: 		"rubyonrails",
-  	enable_starttls_auto: true
+  	:address 	=>	"smtp.gmail.com",
+  	:port 		=>	587,
+  	:domain 	=>	"ffap-itt-2015",
+  	:authentication => "plain",
+  	:user_name 	=>	"ffap.itt.2015@gmail.com",
+  	:password 	=>	"rubyonrails",
+  	:enable_starttls_auto => true
   }
 end
