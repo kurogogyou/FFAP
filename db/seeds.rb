35780ad475cb89c833662e84bccc9ba5e398d9d3
Mario J. Lara <mario.lara1289@gmail.com>
2015-07-23 07:34:17 -0400

    Adding new parts to seeds - progress 10%

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,11 @@ Brand.delete_all
 honda = Brand.create!(brand_name: 'Honda')
 toyo = Brand.create!(brand_name: 'Toyota')
 lex = Brand.create!(brand_name: 'Lexus')
-volk = Brand.create!(brand_name: 'Volkswagen')
+volk = Brand.create!(brand_name: 'Volkswaggen')
 mitsu = Brand.create!(brand_name: 'Mitsubishi')
+mazda = Brand.create!(brand_name: 'Mazda')
+ford = Brand.create!(brand_name: 'Ford')
+niss = Brand.create!(brand_name: 'Nissan')
 
 #modelo = VehicleModel.create!(model_name: 'Civic', year: 2007)
 #modelo.brand = honda
@@ -28,36 +31,175 @@ mitsu = Brand.create!(brand_name: 'Mitsubishi')
 
 	lex.vehicle_models.create!(model_name: 'IS', year: year)
 	lex.vehicle_models.create!(model_name: 'GS', year: year)
-	lex.vehicle_models.create!(model_name: 'LS', year: year)
-	lex.vehicle_models.create!(model_name: 'ES', year: year)
+	#lex.vehicle_models.create!(model_name: 'LS', year: year)
+	#lex.vehicle_models.create!(model_name: 'ES', year: year)
 
 	volk.vehicle_models.create!(model_name: 'Jetta', year: year)
 	volk.vehicle_models.create!(model_name: 'Golf', year: year)
-	volk.vehicle_models.create!(model_name: 'Gol', year: year)
-	volk.vehicle_models.create!(model_name: 'Bora', year: year)
+	#volk.vehicle_models.create!(model_name: 'Gol', year: year)
+	#volk.vehicle_models.create!(model_name: 'Bora', year: year)
 
 	mitsu.vehicle_models.create!(model_name: 'Lancer', year: year)
 	mitsu.vehicle_models.create!(model_name: 'Eclipse', year: year)
+
+	ford.vehicle_models.create!(model_name: 'Focus', year: year)
+	ford.vehicle_models.create!(model_name: 'Mustang', year: year)
+
+	mazda.vehicle_models.create!(model_name: '3', year: year)
+	mazda.vehicle_models.create!(model_name: '6', year: year)
+
+	niss.vehicle_models.create!(model_name: 'Maxima', year: year)
+	niss.vehicle_models.create!(model_name: 'Sentra', year: year)
 end
 #...
 
-honda.vehicle_models.where(
+civ01 = honda.vehicle_models.where(
 	:model_name => 'Civic',
-	 :year => 2007
-).first.products.create!(
-	title: 'Retrovisor',
+	 :year => 2001
+).first
+
+civ01.products.create!(
+	title: 'Aro Civic 01',
 	description: 
-		%{<p>
-			Retrovisor Honda Civic 2007
-		</p>},
-	image_url: 'retro.jpg',
-	price: 12.67,
+		'Aros Honda Civic 2001',
+	image_url: 'Honda Civic 01/Aros Civic 01.PNG',
+	price: 9000,
+	brand_id: honda.id)
+civ01.products.create!(
+	title: 'Bonete Civic 01',
+	description: 
+		'Bonete Honda Civic 2001',
+	image_url: 'Honda Civic 01/Bonete Civic 01.PNG',
+	price: 6600,
+	brand_id: honda.id)
+civ01.products.create!(
+	title: 'Bumper Delantero Civic 01',
+	description: 
+		'Bumper Delantero Honda Civic 2001',
+	image_url: 'Honda Civic 01/Bumper Delantero Civic 01.PNG',
+	price: 3800,
+	brand_id: honda.id)
+civ01.products.create!(
+	title: 'Bumper Trasero Civic 01',
+	description: 
+		'Bumper Trasero Honda Civic 2001',
+	image_url: 'Honda Civic 01/Bumper Trasero Civic 01.PNG',
+	price: 4400,
+	brand_id: honda.id)
+civ01.products.create!(
+	title: 'Halogenos Civic 01',
+	description: 
+		'Halogenos Honda Civic 2001',
+	image_url: 'Honda Civic 01/Halogenos Civic 01.PNG',
+	price: 1500,
+	brand_id: honda.id)
+civ01.products.create!(
+	title: 'Luz Delantera Civic 01',
+	description: 
+		'Luz Delantera Honda Civic 2001',
+	image_url: 'Honda Civic 01/Luz Delantera Civic 01.PNG',
+	price: 2000,
+	brand_id: honda.id)
+civ01.products.create!(
+	title: 'Luz Trasera Civic 01',
+	description: 
+		'Luz Trasera Honda Civic 2001',
+	image_url: 'Honda Civic 01/Luz Trasera Civic 01.PNG',
+	price: 2100,
+	brand_id: honda.id)
+civ01.products.create!(
+	title: 'Parrilla Civic 01',
+	description: 
+		'Parrilla Honda Civic 2001',
+	image_url: 'Honda Civic 01/Parrilla Civic 01.PNG',
+	price: 1800,
+	brand_id: honda.id)
+civ01.products.create!(
+	title: 'Retrovisor Civic 01',
+	description: 
+		'Retrovisor Honda Civic 2001',
+	image_url: 'Honda Civic 01/Retrovisor Civic 01.PNG',
+	price: 2100,
 	brand_id: honda.id)
 
-toyo.vehicle_models.where(
+acc09 = honda.vehicle_models.where(
+	:model_name => 'Accord',
+	 :year => 2009
+).first
+
+acc09.products.create!(
+	title: 'Honda Accord 08 Aros',
+	description: 
+		'Honda Accord Aros 2008',
+	image_url: 'Honda Civic 01/Honda Accord 08 Aros.PNG',
+	price: 9000,
+	brand_id: honda.id)
+acc09.products.create!(
+	title: 'Honda Accord 08 Bonete',
+	description: 
+		'Honda Accord Bonete 2008',
+	image_url: 'Honda Civic 01/Honda Accord 08 Bonete.PNG',
+	price: 6100,
+	brand_id: honda.id)
+acc09.products.create!(
+	title: 'Honda Accord 08 Bumper Delantero',
+	description: 
+		'Honda Accord Bumper Delantero 2008',
+	image_url: 'Honda Civic 01/Honda Accord 08 Bumper Delantero.PNG',
+	price: 4200,
+	brand_id: honda.id)
+acc09.products.create!(
+	title: 'Honda Accord 08 Bumper Trasero',
+	description: 
+		'Honda Accord Bumper Trasero 2008',
+	image_url: 'Honda Civic 01/Honda Accord 08 Bumper Trasero.PNG',
+	price: 5900,
+	brand_id: honda.id)
+acc09.products.create!(
+	title: 'Honda Accord 08 Halogenos',
+	description: 
+		'Honda Accord Halogenos 2008',
+	image_url: 'Honda Civic 01/Honda Accord 08 Halogenos.PNG',
+	price: 3600,
+	brand_id: honda.id)
+acc09.products.create!(
+	title: 'Honda Accord 08 Luz Delantera',
+	description: 
+		'Honda Accord Luz Delantera 2008',
+	image_url: 'Honda Civic 01/Honda Accord 08 Luz Delantera.PNG',
+	price: 5700,
+	brand_id: honda.id)
+acc09.products.create!(
+	title: 'Honda Accord 08 Luz Trasera',
+	description: 
+		'Honda Accord Luz Trasera 2008',
+	image_url: 'Honda Civic 01/Honda Accord 08 Luz Trasera.PNG',
+	price: 3700,
+	brand_id: honda.id)
+acc09.products.create!(
+	title: 'Honda Accord 08 Parrilla',
+	description: 
+		'Honda Accord Parrilla 2008',
+	image_url: 'Honda Civic 01/Honda Accord 08 Parrilla.PNG',
+	price: 2500,
+	brand_id: honda.id)
+acc09.products.create!(
+	title: 'Honda Accord 08 Retrovisor',
+	description: 
+		'Honda Accord Retrovisor 2008',
+	image_url: 'Honda Civic 01/Honda Accord 08 Retrovisor.PNG',
+	price: 2900,
+	brand_id: honda.id)
+
+#######################################
+#Toyota
+
+coro03 = toyo.vehicle_models.where(
 	:model_name => 'Corolla',
 	 :year => 2003
-).first.products.create!(
+).first
+
+coro03.products.create!(
 	title: 'Luz Trasera',
 	description: 
 		%{<p>
