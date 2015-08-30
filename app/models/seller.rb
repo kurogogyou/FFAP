ec56ecf644705752785d3db264710c680abb4ff0
Mario J. Lara <mario.lara1289@gmail.com>
2015-08-30 17:54:01 -0400

    Modified forms for Sellers, added logos

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -2,4 +2,9 @@ class Seller < ActiveRecord::Base
 	has_many :stocks, dependent: :destroy
 	validates :name, presence: true, uniqueness: true
 	validates :address, presence: true
+
+	validates :logo_url, allow_blank: true, format: {
+		with:  %r{\.(gif|jpg|png)\Z}i,
+		message: 'must be a URL for GIF, JPG, or PNG image.' 
+	}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150830184245) do
+ActiveRecord::Schema.define(version: 20150830212559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,10 +77,11 @@ ActiveRecord::Schema.define(version: 20150830184245) do
     t.string   "phone"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "logo_url"
   end
 
   create_table "stocks", force: true do |t|
-    t.integer  "quantity"
+    t.integer  "quantity",    default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "products_id"
