7f6a286d32209db2ae127d8c9bb6c15c724f37d3
Mario J. Lara <mario.lara1289@gmail.com>
2015-10-03 23:11:17 -0400

    Added ratings to reviews

---
+++ db/migrate/20151004024119_adding_ratings_to_reviews.rb
@@ -0,0 +1,5 @@
+class AddingRatingsToReviews < ActiveRecord::Migration
+  def change
+  	add_column :reviews, :rating, :float
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150919221140) do
+ActiveRecord::Schema.define(version: 20151004024119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20150919221140) do
   end
 
   add_index "carts", ["user_id"], name: "index_carts_on_user_id", using: :btree
+
+  create_table "deliveries", force: true do |t|
+    t.decimal  "latitude",   precision: 10, scale: 7
+    t.decimal  "longitude",  precision: 10, scale: 7
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "line_items", force: true do |t|
     t.integer  "product_id"
@@ -79,6 +86,7 @@ ActiveRecord::Schema.define(version: 20150919221140) do
     t.datetime "updated_at"
     t.integer  "seller_id"
     t.integer  "user_id"
+    t.float    "rating"
   end
 
   add_index "reviews", ["seller_id"], name: "index_reviews_on_seller_id", using: :btree
