2652cf7d1e3572c0848461e8a08e2ede0cbba143
Mario J. Lara <mario.lara1289@gmail.com>
2015-10-10 10:53:04 -0400

    Disabled mailer temporarily

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -9,7 +9,7 @@ class OrderNotifier < ActionMailer::Base
   def received(order)
     @order = order
 
-    mail to: order.email, subject: 'Fast Find Auto Parts Order Confirmation'
+  #  mail to: order.email, subject: 'Fast Find Auto Parts Order Confirmation'
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -20,6 +20,6 @@ class OrderNotifier < ActionMailer::Base
   def shipped(order)
     @order = order
 
-    mail to: order.email, subject: 'Fast Find Auto Parts Order Shipped'
+  #  mail to: order.email, subject: 'Fast Find Auto Parts Order Shipped'
   end
 end
