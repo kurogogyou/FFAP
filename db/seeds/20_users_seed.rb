49037b78ebdb50bc56de89d7450306c90c00bcb1
kurogogyou <ml.vizard@gmail.com>
2015-10-19 10:14:08 -0400

    Added a standard locaion to every user in seeds

--- db/seeds/20_users_seed.rb
+++ db/seeds/20_users_seed.rb
@@ -4,8 +4,12 @@
 admin = User.create(:username => "admin", 
 	:password => "1234", 
 	:password_confirmation => "1234", 
-	:email => "admin@example.com")
-admin.update(:role => "admin")
+	:email => "admin@example.com",
+	:role => "admin")
+
+location = Location.create(:latitude => 18.467208,
+	:longitude => -69.911963,
+	:user_id => admin.id)
 
 mario = User.create(:username => "Mario", 
 	:password => "1234", 
@@ -19,29 +23,49 @@ location = Location.create(:latitude => 18.467208,
 frank = User.create(:username => "Frank", 
 	:password => "1234", 
 	:password_confirmation => "1234", 
-	:email => "frank@autoparts.com")
-frank.update(:role => "seller")
+	:email => "frank@autoparts.com",
+	:role => "seller")
+
+location = Location.create(:latitude => 18.467208,
+	:longitude => -69.911963,
+	:user_id => frank.id)
 
 frank = User.create(:username => "Jason", 
 	:password => "1234", 
 	:password_confirmation => "1234", 
-	:email => "jasonx@advance.com")
-frank.update(:role => "seller")
+	:email => "jasonx@advance.com",
+	:role => "seller")
+
+location = Location.create(:latitude => 18.467208,
+	:longitude => -69.911963,
+	:user_id => frank.id)
 
 frank = User.create(:username => "John", 
 	:password => "1234", 
 	:password_confirmation => "1234", 
-	:email => "jbaptist@applus.com")
-frank.update(:role => "seller")
+	:email => "jbaptist@applus.com",
+	:role => "seller")
+
+location = Location.create(:latitude => 18.467208,
+	:longitude => -69.911963,
+	:user_id => frank.id)
 
 frank = User.create(:username => "Tommy", 
 	:password => "1234", 
 	:password_confirmation => "1234", 
-	:email => "tml@autosupply.com")
-frank.update(:role => "seller")
+	:email => "tml@autosupply.com",
+	:role => "seller")
+
+location = Location.create(:latitude => 18.467208,
+	:longitude => -69.911963,
+	:user_id => frank.id)
 
 User.create(:username => "Alin", 
 	:password => "1234", 
 	:password_confirmation => "1234", 
 	:email => "alin@ffap.com",
 	:role => "delivery")
+
+location = Location.create(:latitude => 18.467208,
+	:longitude => -69.911963,
+	:user_id => frank.id)
