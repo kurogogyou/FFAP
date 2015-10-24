0948f79dbdd4e13314b5dea2f495853ce10c22db
Mario J. Lara <mario.lara1289@gmail.com>
2015-10-24 13:02:12 -0400

    Delivery map now visible on controller using param id

--- app/controllers/mapa_controller.rb
+++ app/controllers/mapa_controller.rb
@@ -1,8 +1,15 @@
 class MapaController < ApplicationController
+  before_action :set_delivery, only: [:show]
+
   def index
-  	@delivery = Delivery.find(5)
+  	#@delivery = Delivery.find(5)
   end
 
   def show
+  end
+
+  private
+  def set_delivery
+  	@delivery = Delivery.find(params[:id])
   end
 end
