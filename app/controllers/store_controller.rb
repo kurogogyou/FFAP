fdc03cdeed0f50debd5354a3c12342c8dd332088
Mario J. Lara <mario.lara1289@gmail.com>
2015-10-25 21:51:27 -0400

    Implemented the store search form for query, chassis and registered vehicles

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -9,7 +9,15 @@ class StoreController < ApplicationController
   before_action :set_cart
 
   def index
-    if params[:vehicle] and params[:vehicle] != "" and current_user and current_user.role == 'client'
+    if (!is_empty(params[:chassis_brand]) and !is_empty(params[:chassis_model]) and !is_empty(params[:chassis_year]))
+      brand = Brand.where(:brand_name => params[:chassis_brand]).take
+      model = VehicleModel.where(:model_name => params[:chassis_model]).take
+      if brand == nil or model == nil
+        @products = []
+        return
+      end
+      @products = search_helper("", brand.id, params[:chassis_model], params[:chassis_year])
+    elsif !is_empty(params[:vehicle]) and current_user and current_user.role == 'client'
       vehicle = current_user.vehicles.find(params[:vehicle])
       @products = search_helper("", vehicle.get_brand_id, 
         vehicle.check_model, vehicle.year)
