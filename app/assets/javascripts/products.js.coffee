3edf75729baa530478de059b9bd6bb8a0cd0527f
kurogogyou <ml.vizard@gmail.com>
2015-11-20 11:50:43 -0400

    Applied a searcher to the product listing

--- app/assets/javascripts/products.js.coffee
+++ app/assets/javascripts/products.js.coffee
@@ -1,3 +1,33 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).on "ready, page:change", ->
+  if $('body').hasClass 'products'
+    $('#model_id').attr('disabled', 'disabled')
+    $('#year').attr('disabled', 'disabled')
+
+    $('.products .entry > img').click ->
+      $(this).parent().find(':submit').click()
+
+    $('#brand_id').change ->
+      if $('#brand_id option:selected').text() != 'Select brand'
+        $('#model_id').removeAttr('disabled')
+        $('#year').removeAttr('disabled')
+
+        for option in $('#model_id').children()
+          do ->
+            $(option).css('display', 'none')
+
+        brand = $('#brand_id option:selected').val()
+        options = $('#model_id').children("[brand='"+brand+"']")
+
+        for option in options
+          do ->
+            $(option).css('display', 'block')
+
+      else
+        $('#model_id').attr('disabled', 'disabled')
+        $('#model_id').val('not')
+        $('#year').attr('disabled', 'disabled')
+        $('#year').val('')
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,28 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.order(:id).page params[:page]
+    if (!is_empty(params[:chassis_brand]) and !is_empty(params[:chassis_model]) and !is_empty(params[:chassis_year]))
+      brand = Brand.where(:brand_name => params[:chassis_brand]).take
+      model = VehicleModel.where(:model_name => params[:chassis_model]).take
+      if brand == nil or model == nil
+        @products = Product.where(:title => "n/a").order(:title).page params[:page]
+        return
+      end
+      @products = search_helper(params[:search], brand.id, params[:chassis_model], params[:chassis_year]).order(:title).
+        page params[:page]
+    
+    elsif !is_empty(params[:vehicle]) and current_user and current_user.role == 'client'
+      vehicle = current_user.vehicles.find(params[:vehicle])
+      @products = search_helper(params[:search], vehicle.get_brand_id, 
+        vehicle.check_model, vehicle.year).order(:title).page params[:page]
+
+    elsif params[:search] or params[:brand_id] or params[:model_id] or params[:year]
+      @products = search_helper(params[:search], params[:brand_id], 
+        params[:model_id], params[:year]).order(:title).page params[:page]
+    
+    else
+      @products = Product.order(:title).page params[:page]
+    end
   end
 
   # GET /products/1
