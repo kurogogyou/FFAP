b622ef755a0fc4f261f19d6541de96de53069f65
Mario J. Lara <mario.lara1289@gmail.com>
2015-11-22 20:21:44 -0400

    Google maps always loads now

--- app/assets/javascripts/googlemaps.js
+++ app/assets/javascripts/googlemaps.js
@@ -1,15 +1,24 @@
-//Google Map
 
-var map;
-var marker;
+ready = function() {
+  var initialize, map, marker, markerCoords, script;
+  script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' + 'libraries=places&' + 'callback=initialize';
+  document.body.appendChild(script);
+  return;
+};
 
-function initialize() {
-  var mapCanvas = document.getElementById('mapa');
-  var mapOptions = {
-      center: new google.maps.LatLng(18.463078, -69.929489),
-      zoom: 14,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
+map = void 0;
+marker = void 0;
+
+initialize = function() {
+  var mapCanvas, mapOptions;
+  mapCanvas = document.getElementById('mapa');
+  mapOptions = {
+    center: new google.maps.LatLng(18.463078, -69.929489),
+    zoom: 14,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
   map = new google.maps.Map(mapCanvas, mapOptions);
   marker = new google.maps.Marker({
     position: new google.maps.LatLng(18.463078, -69.929489),
@@ -17,12 +26,15 @@ function initialize() {
     draggable: true
   });
   markerCoords(marker);
-}
-
-function markerCoords(markerobject){
-google.maps.event.addListener(markerobject, 'dragend', function(evt){
-    //alert(evt.latLng.lat() + " " + evt.latLng.lng());
+};
+markerCoords = function(markerobject) {
+  google.maps.event.addListener(markerobject, 'dragend', function(evt) {
     document.getElementById('latitude').value = evt.latLng.lat();
     document.getElementById('longitude').value = evt.latLng.lng();
   });
-}
+};
+
+$(document).ready(ready); //calls for the function we defined above (first loading)
+$(document).on('page:load',ready); // the same but for the other loadings
+
+//google.maps.event.addDomListener(window, 'load', initialize);
