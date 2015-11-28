d9fbefc4144222e834d7d0fcb239771f6f4773be
Mario J. Lara <mario.lara1289@gmail.com>
2015-11-27 20:54:10 -0400

    Included product check in migration

--- db/migrate/20151123175618_add_stocked_status_to_products.rb
+++ db/migrate/20151123175618_add_stocked_status_to_products.rb
@@ -1,5 +1,13 @@
 class AddStockedStatusToProducts < ActiveRecord::Migration
-  def change
+  def up
     add_column :products, :stocked, :boolean, default: false
+
+	Product.all.each do |product|
+		product.check_stock
+	end
+  end
+
+  def down
+  	remove_column :products, :stocked
   end
 end
