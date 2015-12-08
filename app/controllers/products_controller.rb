1d59ccc31912a1959b195f393a06969cf729ac0d
Denisse Lara <dmlaramartin@gmail.com>
2015-12-08 01:44:54 -0400

    Beautify product show and other stuff

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@ class ProductsController < ApplicationController
   # before_action :set_cart
   include VehicleModelsHelper
   include StoreHelper
+  include StocksHelper
   include BrandsHelper
   skip_before_action :authorize, only: [:show]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
